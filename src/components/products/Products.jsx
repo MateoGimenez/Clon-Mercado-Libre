89461f374285d6ab26588e0c0fa3027dba6b60d2
Mateo Gimenez <matex23gimenez11@gmail.com>
2025-09-08 16:18:59 -0300

    Termino el carrusel de productos

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import './products.css'
 
 export const Products = () => {
+  const [indice, setIndice] = useState(0)
   const productos = [
     { motivo: 'Llevate tu favorito', nombre: "Teclado Mecánico Attack Shark X98pro 98% Inalámbrico Gasket", img: './public/products/teclado mecanicoinalambricoAula.webp', precio: 184615, descuento: 41, envio: 'envio gratis' },
     { motivo: 'Llevate tu favorito', nombre: "Teclado Mecánico Attack Shark X98pro 98% Inalámbrico Gasket", img: './public/products/teclado mecanicoinalambricoAula.webp', precio: 184615, descuento: 41, envio: 'envio gratis' },
@@ -8,12 +10,8 @@ export const Products = () => {
     { motivo: 'Llevate tu favorito', nombre: "Teclado Mecánico Attack Shark X98pro 98% Inalámbrico Gasket", img: './public/products/teclado mecanicoinalambricoAula.webp', precio: 184615, descuento: 41, envio: 'envio gratis' },
     { motivo: 'Llevate tu favorito', nombre: "ICONO", img: './public/products/teclado mecanicoinalambricoAula.webp', precio: 184615, descuento: 41, envio: 'envio gratis' },
     { motivo: 'Llevate tu favorito', nombre: "ICONO", img: './public/products/teclado mecanicoinalambricoAula.webp', precio: 184615, descuento: 41, envio: 'envio gratis' },
-    { motivo: 'Comienza otro', nombre: "Teclado Mecánico Attack Shark X98pro 98% Inalámbrico Gasket", img: './public/products/teclado mecanicoinalambricoAula.webp', precio: 184615, descuento: 41, envio: 'envio gratis' },
-    { motivo: 'Comienza otro', nombre: "Teclado Mecánico Attack Shark X98pro 98% Inalámbrico Gasket", img: './public/products/teclado mecanicoinalambricoAula.webp', precio: 184615, descuento: 41, envio: 'envio gratis' },
-    { motivo: 'Comienza otro', nombre: "Teclado Mecánico Attack Shark X98pro 98% Inalámbrico Gasket", img: './public/products/teclado mecanicoinalambricoAula.webp', precio: 184615, descuento: 41, envio: 'envio gratis' },
-    { motivo: 'Comienza otro', nombre: "Teclado Mecánico Attack Shark X98pro 98% Inalámbrico Gasket", img: './public/products/teclado mecanicoinalambricoAula.webp', precio: 184615, descuento: 41, envio: 'envio gratis' },
-    { motivo: 'Comienza otroo', nombre: "ICONO", img: './public/products/teclado mecanicoinalambricoAula.webp', precio: 184615, descuento: 41, envio: 'envio gratis' },
-    { motivo: 'Comienza otro', nombre: "ICONO", img: './public/products/teclado mecanicoinalambricoAula.webp', precio: 184615, descuento: 41, envio: 'envio gratis' }
+    { motivo: 'Comienza otro', nombre: "ICONO", img: './public/products/teclado mecanicoinalambricoAula.webp', precio: 184615, descuento: 41, envio: 'envio gratis' },
+    { motivo: 'Comienza otro', nombre: "ICONO", img: './public/products/teclado mecanicoinalambricoAula.webp', precio: 184615, descuento: 41, envio: 'envio gratis' },
   ]
 
   const productsLimpio = productos.map((item) => ({
@@ -21,16 +19,27 @@ export const Products = () => {
     precioConDescuento: item.precio - (item.precio * item.descuento) / 100
   }))
 
-  const formatearPrecio = (valor) =>
-    new Intl.NumberFormat("es-AR", { style: "currency", currency: "ARS" }).format(valor);
+  const formatearPrecio = (valor) => new Intl.NumberFormat("es-AR", { style: "currency", currency: "ARS" }).format(valor);
+
+  const itemsPorVista = 4
+  const maxIndice = Math.ceil(productos.length / itemsPorVista) - 1
+
+  const siguiente = () => {
+    setIndice((prev) => Math.min(prev + 1, maxIndice))
+  }
+
+  const anterior = () => {
+    setIndice((prev) => Math.max(prev - 1, 0))
+  }
 
   return (
     <div className="contenedor-products">
+      <button className={`boton-izquierda ${indice === 0 ? "oculto" : ""}`} onClick={anterior}>‹</button>
       <div className="carrusel-products">
-        <button className="boton-izquierda">‹</button>
-        <button className="boton-derecha">›</button>
         {productsLimpio.map((item, index) => (
-          <div className="card-products" key={index}>
+          <div className="card-products" key={index}
+            style={{ transform: `translateX(-${indice * 426}%)` }}
+          >
             <h2>{item.motivo}</h2>
             <img src={item.img} alt="imagen producto" />
             <p className='nombre-product'>{item.nombre}</p>
@@ -43,6 +52,8 @@ export const Products = () => {
           </div>
         ))}
       </div>
+      <button className={`boton-derecha ${indice === maxIndice ? "oculto" : ""}`} onClick={siguiente}>›</button>
     </div>
+
   )
 }
