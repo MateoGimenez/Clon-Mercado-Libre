9ccb69ff263e71a51bd4a6057a7bfebbe81ba6ec
Mateo Gimenez <matex23gimenez11@gmail.com>
2025-09-08 21:04:34 -0300

    cambio el import de app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Header } from './components/header/Header'
 import { Home } from './components/home/home'
 import { Products } from './components/products/Products'
-import './App.css'
+import './app.css'
 
 function App() {
   return (
