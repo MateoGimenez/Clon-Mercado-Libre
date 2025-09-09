5b2fc06b9ea8c802997e824b74f729d286e673b2
Mateo Gimenez <matex23gimenez11@gmail.com>
2025-09-08 21:16:53 -0300

    cambio importaciones de campana y carrito

--- src/components/header/carrito/Carrito.jsx
+++ src/components/header/carrito/Carrito.jsx
@@ -2,7 +2,7 @@ import "./carrito.css"
 export const Carrito = () =>{
     return(
         <div className="carrito">
-            <img src="/carrito/carrito.png" alt="" />
+            <img src="/carrito.png" alt="" />
         </div>
     )
 }
